40ce0f7d88b46a8e3241384b04f8ba2b17e20ffa
Kevin Ngoh <kngoh@student.unimelb.edu.au>
2019-09-04 23:28:44 +1000

    Auto stash before rebase of "dev"

--- app/src/main/java/com/example/keepsake/HomePage.java
+++ app/src/main/java/com/example/keepsake/HomePage.java
@@ -2,13 +2,42 @@ package com.example.keepsake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    private Button button;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        button = (Button) findViewById(R.id.button_to_settings);
+        button1 = (Button) findViewById(R.id.button_to_uploads);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (button == findViewById(R.id.button_to_settings)) {
+                    openActivity1();
+                } else if (button1 == findViewById(R.id.button_to_uploads)) {
+                    openActivity2();
+                }
+            }
+        });
+    }
+
+    public void openActivity1() {
+        Intent intent = new Intent(this, AccountSettings.class);
+        startActivity(intent);
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(this, NewItemUpload.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/keepsake/MainActivity.java
+++ app/src/main/java/com/example/keepsake/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.keepsake;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.main_login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
 
+    public void openActivity2() {
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
+    }
 }
