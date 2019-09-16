ae3c3a2af310597b970e3397d22822101d5d9ce3
MarvelJonathan <msulindro@student.unimelb.edu.au>
2019-09-16 15:56:43 +1000

    Navigation Bar Done

--- app/src/main/java/com/example/keepsake/HomePage.java
+++ app/src/main/java/com/example/keepsake/HomePage.java
@@ -1,18 +1,29 @@
 package com.example.keepsake;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePage extends AppCompatActivity {
 
     private Button button;
     private Button button1;
     private Button button2;
+
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,33 @@ public class HomePage extends AppCompatActivity {
                 openActivity2();
             }});
 
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.ButtonHomepageAccess){
+                    Toast.makeText(HomePage.this, "HomePage", Toast.LENGTH_SHORT).show();
+                }
+                else  if (id == R.id.ButtonFamilyItemsAccess){
+                    Toast.makeText(HomePage.this, "FamilyItems", Toast.LENGTH_SHORT).show();
+                }
+                else  if (id == R.id.ButtonFamilyMembersAccess){
+                    Toast.makeText(HomePage.this, "FamilyMembers", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
     }
 
 
@@ -49,5 +87,10 @@ public class HomePage extends AppCompatActivity {
 //        Intent intent = new Intent(this, ViewItem.class);
 //        startActivity(intent);
 //    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
 
 }
