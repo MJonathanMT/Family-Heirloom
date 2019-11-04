7f57c9e932dae83fe247005bee341cef9acb739d
NinaA <averill.nina@gmail.com>
2019-11-04 16:39:16 +1100

    Commenting

--- app/src/main/java/com/example/keepsake/database/firebaseAdapter/FirebaseAdapter.java
+++ app/src/main/java/com/example/keepsake/database/firebaseAdapter/FirebaseAdapter.java
@@ -16,12 +16,23 @@ import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.Map;
 
+/**
+ * This class handles all of the applications interactions with the database. This class
+ * stores the references to the database, storage and authentication portal and handles all calls
+ * to the FirebaseFirestore instance. This is used to standardise access to the database across the
+ *  application.
+ *
+ * */
 public abstract class FirebaseAdapter {
+    //reference to the database instance
     private static FirebaseFirestore db;
+    // reference to the storage portal for images
     private static FirebaseStorage storage;
+    // reference to the authentication portal
     private static FirebaseAuth auth;
 
 
+    // Failure listener =to log all failed calls to the database
     protected static OnFailureListener failureListener = new OnFailureListener() {
         @Override
         public void onFailure(@NonNull Exception e) {
@@ -29,12 +40,22 @@ public abstract class FirebaseAdapter {
         }
     };
 
+    /**
+     * Function to initialise the database
+     * @param ctx       : The current conntext of the application
+     * */
     private static void initialiseDB(Context ctx) {
         FirebaseApp.initializeApp(ctx);
         db = FirebaseFirestore.getInstance();
     }
 
+    /**
+     * Function to standardise access to the database
+     * @param ctx       : The current context of the application
+     * @return          : instance of the database
+     * */
     protected static FirebaseFirestore getDB(Context ctx) {
+        // initialise database
         if (db == null) {
             initialiseDB(ctx);
         }
@@ -42,11 +63,19 @@ public abstract class FirebaseAdapter {
         return db;
     }
 
+    /**
+     * Function to initialise image storage
+     * */
     private static void initialiseStorage() {
         storage = FirebaseStorage.getInstance();
     }
 
+    /**
+     * Function to standardise access to storage
+     * @return      : instance of the storage
+     * */
     protected static FirebaseStorage getStorage() {
+        // initialise storage
         if (storage == null) {
             initialiseStorage();
         }
@@ -54,11 +83,19 @@ public abstract class FirebaseAdapter {
         return storage;
     }
 
+    /**
+     * Function to initialise firebase authentication
+     * */
     private static void initialiseAuth() {
         auth = FirebaseAuth.getInstance();
     }
 
+    /**
+     * Function to standardise access to  firebase authentication
+     * @return      : instance of the firebase authentication
+     * */
     protected static FirebaseAuth getAuth() {
+        // intialise authentication
         if (auth == null) {
             initialiseAuth();
         }
@@ -66,6 +103,13 @@ public abstract class FirebaseAdapter {
         return auth;
     }
 
+    /**
+     * Creates a document in a specified collection
+     * @param ctx              : The current conntext of the application
+     * @param collection       : string indicating the collection to upload the document to
+     * @param listener         : On success listener to call once data has been uploaded
+     * @param data             : Map containing the field-value pairs of the document
+     * */
     protected static void createDocument(Context ctx, String collection, OnSuccessListener listener, Map data){
         getDB(ctx)
                 .collection(collection)
@@ -74,6 +118,13 @@ public abstract class FirebaseAdapter {
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Creates a document in a collection using a Firebase Snapshot
+     * @param ctx              : The current conntext of the application
+     * @param collection       : string indicating the collection to upload the document to
+     * @param listener         : On success listener to call once data has been uploaded
+     * @param data             : Firebase Snapshot storing the values of the document
+     * */
     protected static void createDocument(Context ctx, String collection, OnSuccessListener listener, Object data){
         getDB(ctx)
                 .collection(collection)
@@ -82,6 +133,13 @@ public abstract class FirebaseAdapter {
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Creates a document in a specified collection with a specified ID using a Firebase Snapshot
+     * @param ctx              : The current conntext of the application
+     * @param collection       : string indicating the collection to upload the document to
+     * @param documentID       : Document ID to used to referene the snapshot in the database
+     * @param data             : Firebase Snapshot storing the values of the document
+     * */
     protected static void createDocument(Context ctx, String collection, String documentID, Object data){
         getDB(ctx)
                 .collection(collection)
@@ -90,6 +148,13 @@ public abstract class FirebaseAdapter {
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Gets a document in a specified collection
+     * @param ctx              : The current context of the application
+     * @param collection       : string indicating the collection to upload the document to
+     * @param documentID       : Document ID to used to referene the snapshot in the database
+     * @param listener         : On success listener to call once data has been accessed
+     * */
     protected static void getDocument(Context ctx, String collection, String documentID, OnSuccessListener listener){
         getDB(ctx)
                 .collection(collection)
@@ -99,12 +164,27 @@ public abstract class FirebaseAdapter {
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Gets a document in a specified collection
+     * @param ctx              : The current context of the application
+     * @param collection       : string indicating the collection to upload the document to
+     * @param documentID       : Document ID to used to referene the snapshot in the database
+     * @return                 : reference to the document
+     * */
     protected static DocumentReference getDocument(Context ctx, String collection, String documentID){
         return getDB(ctx)
                 .collection(collection)
                 .document(documentID);
     }
 
+    /**
+     * Updates a field of a document in a specified collection
+     * @param ctx              : The current context of the application
+     * @param collection       : string indicating the collection to upload the document to
+     * @param documentID       : Document ID to used to referene the snapshot in the database
+     * @param field            : The field of the document to change
+     * @param value            : The updated value of the field
+     * */
     protected static void updateDocument(Context ctx, String collection, String documentID, String field, String value){
         getDB(ctx)
                 .collection(collection)
@@ -113,6 +193,15 @@ public abstract class FirebaseAdapter {
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Updates a field of a document in a specified collection
+     * @param ctx              : The current context of the application
+     * @param collection       : string indicating the collection to upload the document to
+     * @param documentID       : Document ID to used to referene the snapshot in the database
+     * @param listener         : On success listener to call once data has been accessed
+     * @param field            : The field of the document to change
+     * @param value            : The updated value of the field
+     * */
     protected static void updateDocument(Context ctx, String collection, String documentID, OnSuccessListener listener, String field, String value){
         getDB(ctx)
                 .collection(collection)
--- app/src/main/java/com/example/keepsake/database/firebaseAdapter/FirebaseAuthAdapter.java
+++ app/src/main/java/com/example/keepsake/database/firebaseAdapter/FirebaseAuthAdapter.java
@@ -16,8 +16,19 @@ import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.Map;
 
+/**
+ * This class that handles all of the applications calls that involve user authentication. This class
+ * stores the references to the firebase authentication portal and handles all calls
+ * to the FirebaseFirestore instance. This is used to standardise the authentication process across
+ * the application.
+ * */
 public abstract class FirebaseAuthAdapter extends FirebaseAdapter{
-
+    /**
+     * Function to verify and sign a user into the application using a previously created user account
+     * @param email       : The users registered email account
+     * @param password    : The users input password
+     * @param listener         : On success listener to call once data has been uploaded
+     * */
     public static void signIn(String email, String password, OnSuccessListener listener){
         getAuth()
                 .signInWithEmailAndPassword(email, password)
@@ -25,10 +36,18 @@ public abstract class FirebaseAuthAdapter extends FirebaseAdapter{
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Function to sign a user out of the application
+     * */
     public static void signOut(){
         getAuth().signOut();
     }
 
+    /**
+     * Function to send an email to the user notifying them of a reset email request
+     * @param email       : The users registered email account
+     * @param listener         : On success listener to call once data has been uploaded
+     * */
     public static void notifyResetPassword(String email, OnSuccessListener listener){
         getAuth()
                 .sendPasswordResetEmail(email)
@@ -36,6 +55,12 @@ public abstract class FirebaseAuthAdapter extends FirebaseAdapter{
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Function to create a user account
+     * @param email       : The users registered email account
+     * @param password    : The users input password
+     * @param listener         : On success listener to call once data has been uploaded
+     * */
     public static void createUser(String email, String password, OnSuccessListener listener){
         getAuth()
                 .createUserWithEmailAndPassword(email, password)
@@ -43,6 +68,10 @@ public abstract class FirebaseAuthAdapter extends FirebaseAdapter{
                 .addOnFailureListener(failureListener);
     }
 
+    /**
+     * Function to delete user account
+     * @param listener         : On success listener to call once data has been uploaded
+     * */
     public static void deleteUser(OnSuccessListener listener){
         getAuth()
                 .getCurrentUser()
@@ -50,10 +79,18 @@ public abstract class FirebaseAuthAdapter extends FirebaseAdapter{
                 .addOnSuccessListener(listener);
     }
 
+    /**
+     * Function to access the Firebase reference to the current user account
+     * @return  : reference to the Firebase User account
+     * */
     public static FirebaseUser getCurrentUser(){
         return getAuth().getCurrentUser();
     }
 
+    /**
+     * Function to access the ID of the current user account
+     * @return  : ID of the current user
+     * */
     public static String getCurrentUserID(){
         return getAuth().getCurrentUser()
                 .getUid();
